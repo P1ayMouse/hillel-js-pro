a2410b50e6b8cd6fc5b33293b86471428afdefd8
Artem <artem@ryzhenko.com>
2024-10-06 15:51:27 +0300

    hw-11-1

---
+++ lesson-11/hw-11-1/main.js
@@ -0,0 +1,25 @@
+let addButton = document.getElementById("addButton");
+
+addButton.addEventListener("click", function() {
+    let div = document.createElement("div");
+    let table = document.createElement("table")
+
+    for (let i = 1; i <= 10; i++) {
+        let row = document.createElement("tr")
+        for (let j = 1; j <= 10; j++) {
+            let col = document.createElement("td")
+
+            let cellText = document.createTextNode(i * j)
+
+            col.appendChild(cellText);
+            row.appendChild(col)
+        }
+        table.appendChild(row);
+    }
+
+    div.appendChild(table);
+    addButton.parentNode.insertBefore(div, addButton.nextSibling);
+    table.setAttribute("border", "1px solid black");
+    table.setAttribute("padding", "10px");
+    table.setAttribute("align", "center");
+})
